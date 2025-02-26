240eb29919a78d09427ad20d6a50e49a238b0878
Nhn2025 <dnhu122025@gmail.com>
2025-02-26 09:10:46 +0700

    operators in Python

---
+++ Basics/operators_in_python.py
@@ -0,0 +1,40 @@
+# Arithmetic operators
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# Assignment operators
+x = 10
+x += 2
+x -= 4
+x /= 4
+x **= 4
+print(x)
+
+# Comparison operators
+
+# Logical operators
+
+# Identity operators
+x = ["nhu", "auto"]
+y = ["huynh nhu", "auto test"]
+
+print(x is y) # false
+print(x == y) # true
+print("auto" in y) # true
+print("auto" not in y) #false
+
+# Bitwise operators
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+x = 0
+y = 1
+print(x & y)
