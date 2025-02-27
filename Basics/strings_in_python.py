0eaa15298bbe98a2d66c29a1fee5299123ea1a80
Nhn2025 <dnhu122025@gmail.com>
2025-02-27 11:26:33 +0700

    string

---
+++ Basics/strings_in_python.py
@@ -0,0 +1,16 @@
+x = "This 'is' a string"
+y = 'this is "string" in \'single\' quotes'
+
+print(x)
+print(y)
+
+z="""
+    This is multiple comment lines
+"""
+
+c='''
+    This is multiple comment lines
+'''
+
+print(c)
+print(z)
